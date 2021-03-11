09d8a7a1ab0e1c723ea3518ab4a17e6d35084b13
Okola44 <mercyimaliokola@gmail.com>
2021-03-11 22:08:07 +0300

    fruitfull

---
+++ conditionals.js
@@ -0,0 +1,35 @@
+
+   console.log ("javascript is fun");
+   
+var isadult=(age>18)?"too young":"old enough"
+var age=12;
+
+console.log(isadult);
+var a=2,b=10;
+if(a<b){
+   console.log("okay")
+}
+console.log(4<3)&&(5>3);
+var myNum1=7;
+var myNum2=8;
+if (myNum1<myNum2){
+   console.log("javascript is fun")
+}
+var c=30,d=60;
+if(c>d){
+   console.log("i am likable")
+}
+else{
+   console.log("i love meru")
+}
+var arr=['mangoes','bananas','3','4','love'];
+console.log(arr.length);
+console.log(arr[0]);
+
+var age=12;
+if (age>=18){
+   console.log("old enough")
+}
+else{
+   console.log("not allowed")
+}
---
+++ val.js
@@ -0,0 +1,27 @@
+var a=[1,2,3];
+var a=[1,2,3];
+console.log(a==a);
+var name="edna";
+name="valary";
+console.log(name)
+var a=[12];
+var b=12;
+console.log(a==b)
+var a=100;
+var b=0;
+console.log(a>b)
+var person={
+    nationality:["kenyan","rwandan","ugandan"],
+    age:20,
+    height:"170 cm",
+    weight:60,
+    name:"lisalab"
+}
+console.log(person.weight,person.name,person.age,person.nationality,person.height)
+var age=18;
+if(age>10){
+    console.log("you are a minor");
+    let newAge=age*10;
+    console.log(newAge);
+}
+console.log(newAge);
